1cfaca2308ce7a730ff87d249eea8f9954da4186
Matti Paksula <matti.paksula@iki.fi>
2018-08-19 18:52:22 +0300

    fix (specs) stderr, not stdout

--- spec/sinatra/silent/silence/toggle_webrick_spec.rb
+++ spec/sinatra/silent/silence/toggle_webrick_spec.rb
@@ -18,7 +18,7 @@ RSpec.describe Sinatra do
     # TODO: e2e/tests/webrick_done.rb tests this correctly
     expect {
       subject.stop!
-    }.not_to output(/WEBrick::HTTPServer#start done/).to_stdout_from_any_process
+    }.not_to output(/WEBrick::HTTPServer#start done/).to_stderr_from_any_process
 
     subject.set :silent_webrick, true
     expect {
